5f1a860deb6fe350e9345e448907f7e13e4796cd
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2018-03-14 09:01:15 +0100

    Unit test of parseSortBy helper

--- api/src/helpers/list.js
+++ api/src/helpers/list.js
@@ -1,11 +1,15 @@
 module.exports = {
-  // TODO: unit test
   parseSortBy(queryVal) {
     const sortBy = queryVal || 'createdAt:DESC';
     const sort = {};
     sortBy.split(',').forEach((sortParam) => {
-      const [sortByField, sortByDirection] = sortParam.split(':');
-      sort[sortByField] = (sortByDirection || '').toLowerCase();
+      const [sortByField, sortByDirection = 'asc'] = sortParam.split(':');
+      if (!['asc', 'desc'].includes(sortByDirection.toLowerCase())) {
+        throw new Error('invalid sort direction - can be asc or desc');
+      }
+      if (sortByField) {
+        sort[sortByField] = sortByDirection.toLowerCase();
+      }
     });
 
     return sort;
