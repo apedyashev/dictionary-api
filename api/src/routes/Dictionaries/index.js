e9eba37d91ea4d4c42a66868df5014b95bea83ab
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2018-03-24 12:57:30 +0100

    Swagger docs for /dictionaries/:id/wordsets

--- api/src/routes/Dictionaries/index.js
+++ api/src/routes/Dictionaries/index.js
@@ -173,9 +173,140 @@ router.patch('/:id', policies.checkJwtAuth, dictionaryController.update);
  */
 router.delete('/:id', policies.checkJwtAuth, dictionaryController.delete);
 
+/**
+ * @swagger
+ *
+ * /dictionaries/:id/wordsets:
+ *   post:
+ *     summary: Adds a new wordset to the dictionary
+ *     tags: [Dictionaries]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - $ref: "#/parameters/AuthorizationHeader"
+ *       - $ref: "#/parameters/id"
+ *       - name: payload
+ *         description: Endpoint's payload.
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: "#/definitions/WordsetPayload"
+ *     responses:
+ *       200:
+ *         description: Ok
+ *         schema:
+ *           $ref: '#/definitions/WordsetResponse'
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Dictionary not found
+ *       422:
+ *         description: Validation error
+ *         schema:
+ *           $ref: '#/definitions/ValidationError'
+ *       500:
+ *         description: Server error
+ *         schema:
+ *           $ref: '#/definitions/ResponseServerError'
+ *
+ */
 router.post('/:id/wordsets', policies.checkJwtAuth, wordsetController.create);
+
+/**
+ * @swagger
+ *
+ * /dictionaries/:id/wordsets/:wordSetId:
+ *   patch:
+ *     summary: Updates an existing wordset
+ *     tags: [Dictionaries]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - $ref: "#/parameters/AuthorizationHeader"
+ *       - $ref: "#/parameters/id"
+ *       - $ref: "#/parameters/wordSetId"
+ *       - name: payload
+ *         description: Endpoint's payload.
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: "#/definitions/WordsetPayload"
+ *     responses:
+ *       200:
+ *         description: Ok
+ *         schema:
+ *           $ref: '#/definitions/WordsetResponse'
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Dictionary not found
+ *       422:
+ *         description: Validation error
+ *         schema:
+ *           $ref: '#/definitions/ValidationError'
+ *       500:
+ *         description: Server error
+ *         schema:
+ *           $ref: '#/definitions/ResponseServerError'
+ *
+ */
 router.patch('/:id/wordsets/:wordSetId', policies.checkJwtAuth, wordsetController.update);
+
+/**
+ * @swagger
+ *
+ * /dictionaries/:id/wordsets/:wordSetId:
+ *   delete:
+ *     summary: Deletes a wordset
+ *     tags: [Dictionaries]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - $ref: "#/parameters/AuthorizationHeader"
+ *       - $ref: "#/parameters/id"
+ *       - $ref: "#/parameters/wordSetId"
+ *     responses:
+ *       200:
+ *         description: Ok
+ *         schema:
+ *           $ref: '#/definitions/WordsetResponse'
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Dictionary or wordset not found
+ *       500:
+ *         description: Server error
+ *         schema:
+ *           $ref: '#/definitions/ResponseServerError'
+ *
+ */
 router.delete('/:id/wordsets/:wordSetId', policies.checkJwtAuth, wordsetController.delete);
+
+/**
+ * @swagger
+ *
+ * /dictionaries/:id/wordsets:
+ *   get:
+ *     summary: Returs list of dictionarie's wordsets
+ *     tags: [Dictionaries]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - $ref: "#/parameters/AuthorizationHeader"
+ *       - $ref: "#/parameters/id"
+ *     responses:
+ *       200:
+ *         description: Ok
+ *         schema:
+ *           $ref: '#/definitions/WordsetsResponse'
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Server error
+ *         schema:
+ *           $ref: '#/definitions/ResponseServerError'
+ *
+ */
 router.get('/:id/wordsets', policies.checkJwtAuth, wordsetController.list);
 
 router.post('/words', policies.checkJwtAuth, wordController.create);
