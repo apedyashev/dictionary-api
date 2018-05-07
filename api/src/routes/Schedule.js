0681af4350e2fd0ee855a84bd41c86933df5143e
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2018-05-07 16:33:11 +0200

    Show learn schedule items starting from current day

--- api/src/routes/Schedule.js
+++ api/src/routes/Schedule.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const moment = require('moment-timezone');
 const {parseSortBy} = require('helpers/list');
 const policies = require('helpers/policies');
 const errorHandler = require('helpers/errorHandler');
@@ -12,7 +13,11 @@ router.get('/', policies.checkJwtAuth, async (req, res) => {
     const perPage = +req.query.perPage || 30;
     const page = +req.query.page || 1;
     const sort = parseSortBy(req.query.sortBy);
-    const query = {owner: req.user.id};
+    // show today's items even if they are in the past
+    const startOfToday = moment()
+      .startOf('day')
+      .toDate();
+    const query = {owner: req.user.id, date: {$gte: startOfToday}};
 
     // NOTE: paginate calls the find() function that triggers post:find hook to populate dictionaries
     const items = await LearningSchedule.paginate(query, {page, limit: perPage, sort});
