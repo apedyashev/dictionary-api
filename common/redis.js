91380257e74eccee375ef3c324f8ddfac34fe47b
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2018-05-30 17:19:22 +0200

    Redis config

--- common/redis.js
+++ common/redis.js
@@ -1,6 +1,6 @@
 const bluebird = require('bluebird');
 const redis = require('redis');
-const config = require('../config');
+const config = require('./config');
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 const redisClient = redis.createClient({host: config.host});
