28cdb1a7be57b8d214d067cadf70312cb233a4f8
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2018-07-17 11:59:47 +0200

    CI debug

--- common/config/test.js
+++ common/config/test.js
@@ -1,5 +1,14 @@
 module.exports = {
-  logger: {
-    level: 'error',
+  baseUrl: 'http://localhost:3002',
+  // TODO: delete
+  mongoose: {
+    dbName: 'dictionary-api-test',
+    server: 'mongodb',
   },
+  logger: {
+    level: 'silly',
+  },
+  redis: {
+    host: 'redis'
+  }
 };
